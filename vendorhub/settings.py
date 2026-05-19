b157d6f6d0b6d8c48beab1b662aa68d89ae41bba
Roshan56-tech <sadiroshan11@gmail.com>
2026-05-19 19:00:16 +0530

    Fix Render deployment settings

--- vendorhub/settings.py
+++ vendorhub/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-4&@48&dsaa!5n9u8tv3_tejti=nlc+f5yx#bu=rin8h$e)3+zs
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["vendorhub-project.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -47,6 +47,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+
 ]
 
 ROOT_URLCONF = 'vendorhub.urls'
@@ -121,8 +124,8 @@ USE_TZ = True
 
 
 # Static and media files
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
